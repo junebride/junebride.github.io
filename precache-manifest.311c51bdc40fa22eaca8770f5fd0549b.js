0d5c9aa800f2c9765e2b08793a51d2167bb49be7
junebride <blazequ3en@gmail.com>
2019-04-24 09:50:01 +0800

    deploy

---
+++ precache-manifest.311c51bdc40fa22eaca8770f5fd0549b.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c40999ced3f664bca3728855a06c785",
+    "url": "/assets/sass/libs/_breakpoints.scss"
+  },
+  {
+    "revision": "14a7f3a4b62c93419068",
+    "url": "/css/app.485dd37d.css"
+  },
+  {
+    "revision": "3672d5c5bcf2855e5027",
+    "url": "/js/chunk-vendors.108f45f9.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2042a4030df6e1dd8e84b6d790de645a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e46e364f8f95d98560a7434e6ff23ed8",
+    "url": "/images/bg.jpg"
+  },
+  {
+    "revision": "ec909cae6214212d339e6a0c04010069",
+    "url": "/images/pic01.jpg"
+  },
+  {
+    "revision": "a04c08bd80719e895d94a2bb5a3ef796",
+    "url": "/index (template).html"
+  },
+  {
+    "revision": "07c319c3e390b03e05af6973a9270c40",
+    "url": "/images/pic06.jpg"
+  },
+  {
+    "revision": "7acb94e1692f901244a6e24d76e621bf",
+    "url": "/images/pic07.jpg"
+  },
+  {
+    "revision": "93d7ec122763508b17571e26c68dfa03",
+    "url": "/images/pic05.jpg"
+  },
+  {
+    "revision": "e10311ef40687375699d8c4e7d8fd6d7",
+    "url": "/images/pic04.jpg"
+  },
+  {
+    "revision": "c2e220dfa740f6276f38d399e04412f3",
+    "url": "/images/pic03.jpg"
+  },
+  {
+    "revision": "242cf1e7d1b3f42011be367a82c66a04",
+    "url": "/images/pic02.jpg"
+  },
+  {
+    "revision": "23f64e91a11119a9c4580cb9b939b3cd",
+    "url": "/images/pic09.jpg"
+  },
+  {
+    "revision": "9b409893e34f5366b78b7798cfa33bce",
+    "url": "/images/pic08.jpg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "77cd9052e1a240210080c682ab0b3026",
+    "url": "/index (Default).html"
+  },
+  {
+    "revision": "aff0992faef6ad79b50ec21e2225a881",
+    "url": "/images/overlay.png"
+  },
+  {
+    "revision": "14a7f3a4b62c93419068",
+    "url": "/js/app.8c429f2d.js"
+  },
+  {
+    "revision": "6dfe0bec9773ae132ad2481aa3ce703c",
+    "url": "/assets/sass/components/_list.scss"
+  },
+  {
+    "revision": "18b62560b49c6e80283a1006b052d21b",
+    "url": "/assets/sass/components/_icons.scss"
+  },
+  {
+    "revision": "78f00710645858274cc6645d57e533a8",
+    "url": "/assets/sass/components/_row.scss"
+  },
+  {
+    "revision": "e4388684ecb89205d3dbd4fd8904747a",
+    "url": "/assets/sass/components/_pagination.scss"
+  },
+  {
+    "revision": "523781d2a4c90aa7851ab420f0a70269",
+    "url": "/assets/sass/components/_section.scss"
+  },
+  {
+    "revision": "2705c1b35a1d5116556c20071163db8e",
+    "url": "/assets/sass/components/_table.scss"
+  },
+  {
+    "revision": "98b622f91c7d76c0f62e8fcc42e89c80",
+    "url": "/assets/sass/layout/_header.scss"
+  },
+  {
+    "revision": "13b2eb8524cecc7c8c8ab6487af56ebc",
+    "url": "/assets/sass/layout/_footer.scss"
+  },
+  {
+    "revision": "83a495f95312160f6bdd29787abce011",
+    "url": "/assets/sass/layout/_main.scss"
+  },
+  {
+    "revision": "ce231a874e4415516de2c91f86eb59da",
+    "url": "/assets/sass/layout/_intro.scss"
+  },
+  {
+    "revision": "b97651397838929ac3b5a26ed942d0e3",
+    "url": "/assets/sass/layout/_nav.scss"
+  },
+  {
+    "revision": "340796b126300934e771a6e3265618c7",
+    "url": "/assets/sass/layout/_navPanel.scss"
+  },
+  {
+    "revision": "7f29e81241cbb932980650b6d94e793a",
+    "url": "/assets/sass/layout/_wrapper.scss"
+  },
+  {
+    "revision": "7fc5f0eb33df2bbfe577cc1405c90062",
+    "url": "/assets/sass/components/_image.scss"
+  },
+  {
+    "revision": "a837918f631c699f1acc54a945abb102",
+    "url": "/assets/sass/libs/_fixed-grid.scss"
+  },
+  {
+    "revision": "5b10d6035b023556a0ae6627d2cde60c",
+    "url": "/assets/sass/libs/_functions.scss"
+  },
+  {
+    "revision": "f3395c442448422278343cf8289a1f96",
+    "url": "/assets/sass/libs/_html-grid.scss"
+  },
+  {
+    "revision": "1ed287f0d99546cc97accf756b7f907b",
+    "url": "/assets/sass/libs/_vars.scss"
+  },
+  {
+    "revision": "de32e3896be1b80183927eed7a31ee7b",
+    "url": "/assets/sass/libs/_mixins.scss"
+  },
+  {
+    "revision": "ad9bab0ab6862fcf79f0239b91146d89",
+    "url": "/assets/sass/libs/_vendor.scss"
+  },
+  {
+    "revision": "753cb68e68ac3bb6e857ebcfa5808a26",
+    "url": "/assets/sass/main.scss"
+  },
+  {
+    "revision": "2eb31cc7290e49dcb535a34d6cba530c",
+    "url": "/assets/sass/noscript.scss"
+  },
+  {
+    "revision": "269550530cc127b6aa5a35925a7de6ce",
+    "url": "/assets/css/font-awesome.min.css"
+  },
+  {
+    "revision": "0d3e79ba5577e607379f68002bd6320a",
+    "url": "/assets/sass/components/_icon.scss"
+  },
+  {
+    "revision": "c07298dd19048a8a69ad97e754dfe8d0",
+    "url": "/assets/js/browser.min.js"
+  },
+  {
+    "revision": "ae53a1dbb3ad5832191afb671e2bedb1",
+    "url": "/assets/js/util.js"
+  },
+  {
+    "revision": "a96aa20c88f0dca2110481ea59d3defe",
+    "url": "/assets/sass/components/_form.scss"
+  },
+  {
+    "revision": "2d9bbbb08e0838da776940b8fc133ba5",
+    "url": "/assets/sass/components/_box.scss"
+  },
+  {
+    "revision": "5d556eb2fb7c356fe2d6589a4cb004d6",
+    "url": "/assets/sass/components/_button.scss"
+  },
+  {
+    "revision": "7be4858a55f6dd1796c6a645e2b835d2",
+    "url": "/assets/sass/components/_actions.scss"
+  },
+  {
+    "revision": "44aae981e9a40d9beec50c7d8cf2e1cf",
+    "url": "/assets/sass/base/_typography.scss"
+  },
+  {
+    "revision": "b2c0f3cfd59bd742b4cf3bf92698c414",
+    "url": "/assets/sass/base/_reset.scss"
+  },
+  {
+    "revision": "e10468ada266cf5a04dcd9444d55d90f",
+    "url": "/assets/sass/base/_page.scss"
+  },
+  {
+    "revision": "a09e13ee94d51c524b7e2a728c7d4039",
+    "url": "/assets/js/jquery.min.js"
+  },
+  {
+    "revision": "81a479eb099e3b187613943b085923b8",
+    "url": "/assets/js/breakpoints.min.js"
+  },
+  {
+    "revision": "1ed5a78bde1476875a40f6b9ff44fc14",
+    "url": "/assets/js/jquery.scrolly.min.js"
+  },
+  {
+    "revision": "f89065e3d988006af9791b44561d7c90",
+    "url": "/assets/js/jquery.scrollex.min.js"
+  },
+  {
+    "revision": "f8be13daab920918318a6bc0ab1eb9ec",
+    "url": "/assets/js/main.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/assets/fonts/fontawesome-webfont.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/assets/fonts/fontawesome-webfont.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/assets/fonts/fontawesome-webfont.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/assets/fonts/fontawesome-webfont.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/assets/fonts/fontawesome-webfont.eot"
+  },
+  {
+    "revision": "0d2717cd5d853e5c765ca032dfd41a4d",
+    "url": "/assets/fonts/FontAwesome.otf"
+  },
+  {
+    "revision": "d50fdec12b8c4ed178e2f59b0b30b706",
+    "url": "/assets/css/noscript.css"
+  },
+  {
+    "revision": "b38b6cac1db077420b9bc6dfc23f1a86",
+    "url": "/assets/css/main.css"
+  }
+];
