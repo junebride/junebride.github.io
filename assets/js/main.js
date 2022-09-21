c34482c8a14d4ed9691fef0181fa8fd3e5ebf7cb
junebride <blazequ3en@gmail.com>
2022-09-21 21:39:48 +0800

    init page 2.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,232 +1,159 @@
 /*
-	Massively by HTML5 UP
+	Read Only by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
 (function($) {
-  var $window = $(window),
-    $body = $("body"),
-    $wrapper = $("#wrapper"),
-    $header = $("#header"),
-    $nav = $("#nav"),
-    $main = $("#main"),
-    $navPanelToggle,
-    $navPanel,
-    $navPanelInner;
 
-  // Breakpoints.
-  breakpoints({
-    default: ["1681px", null],
-    xlarge: ["1281px", "1680px"],
-    large: ["981px", "1280px"],
-    medium: ["737px", "980px"],
-    small: ["481px", "736px"],
-    xsmall: ["361px", "480px"],
-    xxsmall: [null, "360px"]
-  });
+	var $window = $(window),
+		$body = $('body'),
+		$header = $('#header'),
+		$titleBar = null,
+		$nav = $('#nav'),
+		$wrapper = $('#wrapper');
 
-  /**
-   * Applies parallax scrolling to an element's background image.
-   * @return {jQuery} jQuery object.
-   */
-  $.fn._parallax = function(intensity) {
-    var $window = $(window),
-      $this = $(this);
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '1025px',  '1280px' ],
+			medium:   [ '737px',   '1024px' ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ null,      '480px'  ],
+		});
 
-    if (this.length == 0 || intensity === 0) return $this;
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
 
-    if (this.length > 1) {
-      for (var i = 0; i < this.length; i++) $(this[i])._parallax(intensity);
+	// Tweaks/fixes.
 
-      return $this;
-    }
+		// Polyfill: Object fit.
+			if (!browser.canUse('object-fit')) {
 
-    if (!intensity) intensity = 0.25;
+				$('.image[data-position]').each(function() {
 
-    $this.each(function() {
-      var $t = $(this),
-        $bg = $('<div class="bg"></div>').appendTo($t),
-        on,
-        off;
+					var $this = $(this),
+						$img = $this.children('img');
 
-      on = function() {
-        $bg.removeClass("fixed").css("transform", "matrix(1,0,0,1,0,0)");
+					// Apply img as background.
+						$this
+							.css('background-image', 'url("' + $img.attr('src') + '")')
+							.css('background-position', $this.data('position'))
+							.css('background-size', 'cover')
+							.css('background-repeat', 'no-repeat');
 
-        $window.on("scroll._parallax", function() {
-          var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
+					// Hide img.
+						$img
+							.css('opacity', '0');
 
-          $bg.css("transform", "matrix(1,0,0,1,0," + pos * intensity + ")");
-        });
-      };
+				});
 
-      off = function() {
-        $bg.addClass("fixed").css("transform", "none");
+			}
 
-        $window.off("scroll._parallax");
-      };
+	// Header Panel.
 
-      // Disable parallax on ..
-      if (
-        browser.name == "ie" || // IE
-        browser.name == "edge" || // Edge
-        window.devicePixelRatio > 1 || // Retina/HiDPI (= poor performance)
-        browser.mobile
-      )
-        // Mobile devices
-        off();
-      // Enable everywhere else.
-      else {
-        breakpoints.on(">large", on);
-        breakpoints.on("<=large", off);
-      }
-    });
+		// Nav.
+			var $nav_a = $nav.find('a');
 
-    $window
-      .off("load._parallax resize._parallax")
-      .on("load._parallax resize._parallax", function() {
-        $window.trigger("scroll");
-      });
+			$nav_a
+				.addClass('scrolly')
+				.on('click', function() {
 
-    return $(this);
-  };
+					var $this = $(this);
 
-  // Play initial animations on page load.
-  $window.on("load", function() {
-    window.setTimeout(function() {
-      $body.removeClass("is-preload");
-    }, 100);
-  });
+					// External link? Bail.
+						if ($this.attr('href').charAt(0) != '#')
+							return;
 
-  // Scrolly.
-  $(".scrolly").scrolly();
+					// Deactivate all links.
+						$nav_a.removeClass('active');
 
-  // Background.
-  $wrapper._parallax(0.925);
+					// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+						$this
+							.addClass('active')
+							.addClass('active-locked');
 
-  // Nav Panel.
+				})
+				.each(function() {
 
-  // Toggle.
-  //   $navPanelToggle = $(
-  //     '<a href="#navPanel" id="navPanelToggle">Menu</a>'
-  //   ).appendTo($wrapper);
+					var	$this = $(this),
+						id = $this.attr('href'),
+						$section = $(id);
 
-  // Change toggle styling once we've scrolled past the header.
-  $header.scrollex({
-    bottom: "5vh",
-    enter: function() {
-      $navPanelToggle.removeClass("alt");
-    },
-    leave: function() {
-      $navPanelToggle.addClass("alt");
-    }
-  });
+					// No section for this link? Bail.
+						if ($section.length < 1)
+							return;
 
-  // Panel.
-  //   $navPanel = $(
-  //     '<div id="navPanel">' +
-  //       "<nav>" +
-  //       "</nav>" +
-  //       '<a href="#navPanel" class="close"></a>' +
-  //       "</div>"
-  //   )
-  //     .appendTo($body)
-  //     .panel({
-  //       delay: 500,
-  //       hideOnClick: true,
-  //       hideOnSwipe: true,
-  //       resetScroll: true,
-  //       resetForms: true,
-  //       side: "right",
-  //       target: $body,
-  //       visibleClass: "is-navPanel-visible"
-  //     });
+					// Scrollex.
+						$section.scrollex({
+							mode: 'middle',
+							top: '5vh',
+							bottom: '5vh',
+							initialize: function() {
 
-  // $navPanel = $("#navPanel").panel({
-  // 	delay: 500,
-  // 	hideOnClick: true,
-  // 	hideOnSwipe: true,
-  // 	resetScroll: true,
-  // 	resetForms: true,
-  // 	side: "right",
-  // 	target: $body,
-  // 	visibleClass: "is-navPanel-visible"
-  // });
+								// Deactivate section.
+									$section.addClass('inactive');
 
-  //   // Get inner.
-  //   $navPanelInner = $navPanel.children("nav");
+							},
+							enter: function() {
 
-  //   // Move nav content on breakpoint change.
-  //   var $navContent = $nav.children();
+								// Activate section.
+									$section.removeClass('inactive');
 
-  //   breakpoints.on(">medium", function() {
-  //     // NavPanel -> Nav.
-  //     $navContent.appendTo($nav);
+								// No locked links? Deactivate all links and activate this section's one.
+									if ($nav_a.filter('.active-locked').length == 0) {
 
-  //     // Flip icon classes.
-  //     $nav.find(".icons, .icon").removeClass("alt");
-  //   });
+										$nav_a.removeClass('active');
+										$this.addClass('active');
 
-  //   breakpoints.on("<=medium", function() {
-  //     // Nav -> NavPanel.
-  //     $navContent.appendTo($navPanelInner);
+									}
 
-  //     // Flip icon classes.
-  //     $navPanelInner.find(".icons, .icon").addClass("alt");
-  //   });
+								// Otherwise, if this section's link is the one that's locked, unlock it.
+									else if ($this.hasClass('active-locked'))
+										$this.removeClass('active-locked');
 
-  // Hack: Disable transitions on WP.
-  if (browser.os == "wp" && browser.osVersion < 10)
-    $navPanel.css("transition", "none");
+							}
+						});
 
-  // Intro.
-  var $intro = $("#intro");
+				});
 
-  if ($intro.length > 0) {
-    // Hack: Fix flex min-height on IE.
-    if (browser.name == "ie") {
-      $window
-        .on("resize.ie-intro-fix", function() {
-          var h = $intro.height();
+		// Title Bar.
+			$titleBar = $(
+				'<div id="titleBar">' +
+					'<a href="#header" class="toggle"></a>' +
+					'<span class="title">' + $('#logo').html() + '</span>' +
+				'</div>'
+			)
+				.appendTo($body);
 
-          if (h > $window.height()) $intro.css("height", "auto");
-          else $intro.css("height", h);
-        })
-        .trigger("resize.ie-intro-fix");
-    }
+		// Panel.
+			$header
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'right',
+					target: $body,
+					visibleClass: 'header-visible'
+				});
 
-    // Hide intro on scroll (> small).
-    breakpoints.on(">small", function() {
-      $main.unscrollex();
+	// Scrolly.
+		$('.scrolly').scrolly({
+			speed: 1000,
+			offset: function() {
 
-      $main.scrollex({
-        mode: "bottom",
-        top: "25vh",
-        bottom: "-50vh",
-        enter: function() {
-          $intro.addClass("hidden");
-        },
-        leave: function() {
-          $intro.removeClass("hidden");
-        }
-      });
-    });
+				if (breakpoints.active('<=medium'))
+					return $titleBar.height();
 
-    // Hide intro on scroll (<= small).
-    breakpoints.on("<=small", function() {
-      $main.unscrollex();
+				return 0;
 
-      $main.scrollex({
-        mode: "middle",
-        top: "15vh",
-        bottom: "-15vh",
-        enter: function() {
-          $intro.addClass("hidden");
-        },
-        leave: function() {
-          $intro.removeClass("hidden");
-        }
-      });
-    });
-  }
+			}
+		});
+
 })(jQuery);
